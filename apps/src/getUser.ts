722d78f68f0ff1e7809bcc8e3325bf5f76654595
pravinnavale <145791759+pravinnavale@users.noreply.github.com>
2023-09-28 16:17:16 +0200

    find function for getUser

--- apps/src/getUser.ts
+++ apps/src/getUser.ts
@@ -3,31 +3,30 @@ import { users } from './router';
 import { simulatedDay } from './main';
 
 export function getUser(req, res) {
-  console.log("REquest received.." + users);
   //const accID = req.query.accountId; //Use to read query parameters
   const accID = req.params['accountId']; // Use to read path parameters
   console.log(accID);
   
   let resp : User;
-  // const user = users.find(user => user.uuid === accID)
-  // console.log("user found: "+ user);
+   const tempUser = users.find(resp => resp.uuid === accID)
+   console.log("user found: "+ tempUser);
 
-  for (const usr of users){
-    if(usr.uuid === accID){
-      if(usr.simulatedDay === simulatedDay){
-        const tmpUsr = {...usr};
-        tmpUsr.balance = tmpUsr.balance - tmpUsr.underProcessBal;
-        resp = tmpUsr;
-      }else{
-        usr.underProcessBal = 0; // Assuming request will never be backdated..
-        resp = usr;
-      }
-      break;      
-    }
-  } 
+  // for (const usr of users){
+  //   if(usr.uuid === accID){
+  //     if(usr.simulatedDay === simulatedDay){
+  //       const tmpUsr = {...usr};
+  //       tmpUsr.balance = tmpUsr.balance - tmpUsr.underProcessBal;
+  //       resp = tmpUsr;
+  //     }else{
+  //       usr.underProcessBal = 0; // Assuming request will never be backdated..
+  //       resp = usr;
+  //     }
+  //     break;      
+  //   }
+  // } 
   
-  if(resp !== undefined){
-    res.status(200).json(resp);
+  if(tempUser !== undefined){
+    res.status(200).json(tempUser);
   }else{
     res.status(400).send('No user found with given account ID: ' + accID);
   }
